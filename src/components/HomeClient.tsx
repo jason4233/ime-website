f6208edd4253d1e9ef3e02b1a03913de67158247
IME Dev <dev@ime-beauty.com>
2026-04-20 02:37:49 +0800

    debug: Hero + FirstVisitQuiz only

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -2,19 +2,14 @@
 
 import { HeroSection } from "@/components/sections/HeroSection";
 import { FirstVisitQuiz } from "@/components/sections/quiz/FirstVisitQuiz";
-import { BrandStorySection } from "@/components/sections/BrandStorySection";
-import { FounderSection } from "@/components/sections/FounderSection";
-// import { ProductSection } from "@/components/sections/ProductSection";
-// import { SkinLayers } from "@/components/sections/SkinLayers";
-// import { CourseSection } from "@/components/sections/CourseSection";
+// import { BrandStorySection } from "@/components/sections/BrandStorySection";
+// import { FounderSection } from "@/components/sections/FounderSection";
 
 export default function HomeClient() {
   return (
     <>
       <HeroSection />
       <FirstVisitQuiz />
-      <BrandStorySection />
-      <FounderSection />
     </>
   );
 }
