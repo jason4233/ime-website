704ccac4080ce8b98e322b6acba34d46d8b09b40
IME Dev <dev@ime-beauty.com>
2026-04-20 02:34:36 +0800

    debug: HomeClient = first 7 sections

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { HeroSection } from "@/components/sections/HeroSection";
-// import { FirstVisitQuiz } from "@/components/sections/quiz/FirstVisitQuiz";
-// import { BrandStorySection } from "@/components/sections/BrandStorySection";
-// import { FounderSection } from "@/components/sections/FounderSection";
-// import { ProductSection } from "@/components/sections/ProductSection";
-// import { SkinLayers } from "@/components/sections/SkinLayers";
-// import { CourseSection } from "@/components/sections/CourseSection";
+import { FirstVisitQuiz } from "@/components/sections/quiz/FirstVisitQuiz";
+import { BrandStorySection } from "@/components/sections/BrandStorySection";
+import { FounderSection } from "@/components/sections/FounderSection";
+import { ProductSection } from "@/components/sections/ProductSection";
+import { SkinLayers } from "@/components/sections/SkinLayers";
+import { CourseSection } from "@/components/sections/CourseSection";
 // import { RnDSection } from "@/components/sections/RnDSection";
 // import { FactorySection } from "@/components/sections/FactorySection";
 // import { CertificateSection } from "@/components/sections/CertificateSection";
@@ -19,6 +19,12 @@ export default function HomeClient() {
   return (
     <>
       <HeroSection />
+      <FirstVisitQuiz />
+      <BrandStorySection />
+      <FounderSection />
+      <ProductSection />
+      <SkinLayers />
+      <CourseSection />
     </>
   );
 }
